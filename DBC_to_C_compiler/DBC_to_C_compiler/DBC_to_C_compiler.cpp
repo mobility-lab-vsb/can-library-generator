b0a588ad85267d24213ae636beb62e5c757c52e5
ryc0091 <ryc0091@vsb.cz>
2024-11-19 18:38:38 +0100

    Structures for signal and messages created

--- DBC_to_C_compiler/DBC_to_C_compiler/DBC_to_C_compiler.cpp
+++ DBC_to_C_compiler/DBC_to_C_compiler/DBC_to_C_compiler.cpp
@@ -1,10 +1,35 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+// Struktura pro signál
+struct DBCSignal {
+    std::string name;
+    int startBit;
+    int length;
+    std::string byteOrder;
+    char valueType;
+    double factor;
+    double offset;
+    double min;
+    double max;
+    std::string unit;
+    std::string receiver;
+};
+
+// Struktura pro zprávu
+struct DBCMessage {
+    int id;
+    std::string name;
+    int dlc;
+    std::string sender;
+    std::vector<DBCSignal> signals;
+};
 
 // Funkce pro otevření souboru .dbc a následné vypsání do konzole pro kontrolu
 void openDBCFile(const std::string& path) {
-    std::ifstream dbcFile(path);
+    std::ifstream dbcFile(path); // Otevření souboru
 
     if (!dbcFile.is_open()) {
         std::cerr << "Error: Failed to open " << path << std::endl;
@@ -15,11 +40,12 @@ void openDBCFile(const std::string& path) {
 
     std::string line;
 
+    // Výpis souboru .dbc na konzoli
     while (std::getline(dbcFile, line)) {
         std::cout << line << std::endl;
     }
 
-    dbcFile.close();
+    dbcFile.close(); // Zavření souboru
 }
 
 int main() {
