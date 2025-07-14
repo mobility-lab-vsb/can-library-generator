67646948e8fff6be2278c034cbf5f0811f7cc398
ryc0091 <ryc0091@vsb.cz>
2025-07-14 16:15:19 +0200

    Renamed project bcs of Briefcase 3

--- src/canlibrarygenerator/test/test_pipeline.py
+++ src/canlibrarygenerator/test/test_pipeline.py
@@ -3,8 +3,8 @@ import subprocess
 import cantools
 import shutil
 import sys
-from src.can-library-generator.generate_functions.generate_c_library import generate_c_code
-from src.can-library-generator.generate_functions.generate_cpp_library import generate_cpp_code
+from src.canlibrarygenerator.generate_functions.generate_c_library import generate_c_code
+from src.canlibrarygenerator.generate_functions.generate_cpp_library import generate_cpp_code
 from collections import namedtuple
 
 # Configuration
